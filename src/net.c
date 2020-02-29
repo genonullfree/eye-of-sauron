1d015be89e40f57fa610fb18c09a2df715eb9265
geno <stephen.halwes@gmail.com>
2020-02-28 18:51:45 -0800

    working multicast

--- src/net.c
+++ src/net.c
@@ -7,17 +7,9 @@ uint8_t initialize_net()
 {
     _net.name = "EYE";
     memcpy(_net.dev_name, "ens33", 6);
-    //_net.local_ip = (union inet_addr)htonl(0x7f000001);
-    _net.remote_ip = (union inet_addr)htonl(0xac103401);
-    //_net.local_port = 1337;
+    _net.remote_ip = (union inet_addr)htonl(0xffffffff);
     _net.remote_port = 1337;
-    //memset(_net.remote_mac, 0xff, ETH_ALEN);
-    _net.remote_mac[0] = 0x00;
-    _net.remote_mac[1] = 0x50;
-    _net.remote_mac[2] = 0x56;
-    _net.remote_mac[3] = 0xc0;
-    _net.remote_mac[4] = 0x00;
-    _net.remote_mac[5] = 0x08;
+    memset(_net.remote_mac, 0xff, ETH_ALEN);
 
     /* debug print options */
     netpoll_print_options(&_net);
