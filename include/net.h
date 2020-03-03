b6f6adf234e5a6e72d92a2cf636fd3d1fcf0b4e2
geno <stephen.halwes@gmail.com>
2020-03-02 18:37:19 -0800

    added code to create a list of network device names

--- include/net.h
+++ include/net.h
@@ -6,6 +6,12 @@
 #include <linux/inet.h>
 #include <linux/netdevice.h>
 
+struct netdevs {
+    char name[16];
+    struct list_head node;
+};
+
+uint8_t scan_netdev(void);
 uint8_t initialize_net(void);
 uint8_t finalize_net(void);
 uint8_t send_packet(char ch);
--- src/net.c
+++ src/net.c
@@ -2,19 +2,35 @@
 #include "../include/ops.h"
 
 static struct netpoll _net;
+static LIST_HEAD(_netdevs);
 
-uint8_t initialize_net()
+uint8_t scan_netdev()
 {
-
     struct net_device *dev;
+    struct netdevs *nd = NULL;
+
     read_lock(&dev_base_lock);
     dev = first_net_device(&init_net);
     while (dev)
     {
         printk(KERN_INFO "found %s\n", dev->name);
+        nd = kmalloc(sizeof(struct netdevs), GFP_KERNEL);
+        if (nd == NULL)
+            return 1;
+
+        strncpy(nd->name,dev->name, 15);
+        list_add(&nd->node, &_netdevs);
+
         dev = next_net_device(dev);
     }
+
     read_unlock(&dev_base_lock);
+    return 0;
+}
+
+uint8_t initialize_net()
+{
+    scan_netdev();
 
     _net.name = "EYE";
     memcpy(_net.dev_name, "ens33", 6);
@@ -35,7 +51,18 @@ uint8_t initialize_net()
 
 uint8_t finalize_net()
 {
+    struct netdevs *tmp_dev = NULL;
+    struct netdevs *ptr_dev = NULL;
+
     netpoll_cleanup(&_net);
+
+    list_for_each_entry_safe(ptr_dev, tmp_dev, &_netdevs, node)
+    {
+        printk("freeing dev: %s\n", ptr_dev->name);
+        list_del(&ptr_dev->node);
+        kfree(ptr_dev);
+    }
+
     return 0;
 }
 
